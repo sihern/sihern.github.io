1a63898935f8b49c6ec4023511bc8d34da6aee08
Sihern <sihern.han@gmail.com>
2021-04-05 18:41:16 -0700

    Day 3: Created board with lists of squares

--- amazons.js
+++ amazons.js
@@ -3,20 +3,30 @@ var canvas_size = 600;
 var square_length = canvas_size / board_size;
 var board_color_dark = 130;
 var board_color_light = 255;
+var board = [];
 
 function setup() {  
     createCanvas(canvas_size, canvas_size);
     background(255, 0, 0, 128);
     create_board();
+    draw_board();
     create_border();
 }
 function create_board(){
-    //draw board
-    draw_board();
-
-    //draw numbers
-    // draw_numbers();
-    //TODO: draw letters
+    for (var y_value = 0; y_value < board_size; y_value++) {
+        var board_row = [];
+        for (var x_value = 0; x_value < board_size; x_value++) {
+            var fill;
+            if ( (x_value + y_value) % 2 == 0) {
+                fill = board_color_light; //set fill to white
+            } else {
+                fill = board_color_dark; //set fill to black
+            }
+            var square = new Square (x_value,y_value, fill);  
+            board_row.push(square);
+        }
+        board.push(board_row);
+    } 
 }
 // function draw_numbers(){
 //     for(var i = 0; i <=)
@@ -32,37 +42,22 @@ function draw_board(){
     noStroke();
     for (var y_value = 0; y_value < board_size; y_value++) {
         for (var x_value = 0; x_value < board_size; x_value++) {
-            if ( (x_value + y_value) % 2 == 0) {
-                fill(board_color_light); //set fill to white
-            } else {
-                fill(board_color_dark); //set fill to black
-            }
-            draw_square(x_value,y_value);
+            var cur_square = board[y_value][x_value];
+            draw_square(cur_square);
            
         }
     }
 }
 
-function draw_square(x, y) {
-    rect(x * square_length, y * square_length, square_length, square_length);
+function draw_square(cur_square) {
+    fill(cur_square.fill_shade);
+    rect(cur_square.x * cur_square.length, cur_square.y * cur_square.length, cur_square.length, cur_square.length);
 }
-var frame_count = 0;
-var rate = 2;
 
-// function draw() {
-//     frame_count = frame_count + 1;
-//     print(frame_count);
-//     if (frame_count % rate == 0) {
-//       draw_it();  
-//     } else {  
-//     }
-// }
-
-// function draw_it() {
-//     if (mouseIsPressed) {
-//         fill(0);
-//     } else {
-//         fill(255);
-//     }
-//     ellipse(mouseX, mouseY, 40, 40);
-// }
+function Square(x, y, fill_shade) {
+    this.x = x;
+    this.y = y;
+    this.length = square_length; 
+    //either dark or light
+    this.fill_shade = fill_shade;
+}
